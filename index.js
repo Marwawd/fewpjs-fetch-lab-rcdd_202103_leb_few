6511fc345c6ffed691ab1662fda40b78d383596a
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-08 16:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch('https://anapioficeandfire.com/api/books')
+   .then(resp => resp.json())
+   .then(json => renderBooks(json))
 
+  /*fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json.character(1031)));}*/
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
+    console.log(book.name);
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
@@ -14,4 +19,6 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
+
 });
