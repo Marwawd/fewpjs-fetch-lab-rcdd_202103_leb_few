f182f1138cdc1ddeff84d1f4be5a7796ddfe11ff
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-08 16:12:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   fetch('https://anapioficeandfire.com/api/books')
    .then(resp => resp.json())
-   .then(json => renderBooks(json))
+   .then(json => 
+//console.log(json[5].name);
+renderBooks(json))
 
   /*fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => console.log(json.character(1031)));}*/
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+for(let book in books){
     const h2 = document.createElement('h2');
-    console.log(book.name);
-    h2.innerHTML = book.name;
+    console.log(books[book].name);
+    h2.innerHTML = books[book].name;
     main.appendChild(h2);
-  });
-}
+  }
+}}
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
